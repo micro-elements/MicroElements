0a690b5ee5dce324dd374a0e365f595da3f644fd
Alexey Petryashev <petriashev@gmail.com>
2020-04-15 19:17:14 +0300

    devops updated

--- build.sh
+++ build.sh
@@ -1,117 +1,81 @@
 #!/usr/bin/env bash
 
 ##########################################################################
-# This is the Cake bootstrapper script for Linux and OS X.
-# This file was downloaded from https://github.com/cake-build/resources
-# Feel free to change this file to fit your needs.
+# This is the modified Cake bootstrapper script for Linux and OS X.
 ##########################################################################
+
+echo "Starting build.sh"
+
+DEVOPS_VERSION=1.9.1
+NUGET_URL=https://api.nuget.org/v3/index.json
 
 # Define directories.
 SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 TOOLS_DIR=$SCRIPT_DIR/tools
-ADDINS_DIR=$TOOLS_DIR/Addins
-MODULES_DIR=$TOOLS_DIR/Modules
-NUGET_EXE=$TOOLS_DIR/nuget.exe
-CAKE_EXE=$TOOLS_DIR/Cake/Cake.exe
-PACKAGES_CONFIG=$TOOLS_DIR/packages.config
-PACKAGES_CONFIG_MD5=$TOOLS_DIR/packages.config.md5sum
-ADDINS_PACKAGES_CONFIG=$ADDINS_DIR/packages.config
-MODULES_PACKAGES_CONFIG=$MODULES_DIR/packages.config
 
-# Define md5sum or md5 depending on Linux/OSX
-MD5_EXE=
-if [[ "$(uname -s)" == "Darwin" ]]; then
-    MD5_EXE="md5 -r"
-else
-    MD5_EXE="md5sum"
-fi
+# Script to run.
+SCRIPT=$TOOLS_DIR/microelements.devops/$DEVOPS_VERSION/scripts/main.cake
 
-# Define default arguments.
-SCRIPT="build.cake"
+CAKE_PROPS_PATH=$TOOLS_DIR/cake.props
+CAKE_VERSION="0.35.0"
 CAKE_ARGUMENTS=()
 
 # Parse arguments.
 for i in "$@"; do
     case $1 in
         -s|--script) SCRIPT="$2"; shift ;;
+        --cake-version) CAKE_VERSION="--version=$2"; shift ;;
         --) shift; CAKE_ARGUMENTS+=("$@"); break ;;
         *) CAKE_ARGUMENTS+=("$1") ;;
     esac
     shift
 done
 
+CAKE_ARGUMENTS+=("--rootDir=$SCRIPT_DIR");
+CAKE_ARGUMENTS+=("--devOpsVersion=$DEVOPS_VERSION");
+CAKE_ARGUMENTS+=("--devOpsRoot=$TOOLS_DIR/microelements.devops/$DEVOPS_VERSION");
+
+echo "===========VARIABLES============"
+echo "SCRIPT_DIR: $SCRIPT_DIR"
+echo "SCRIPT: $SCRIPT"
+echo "TOOLS_DIR: $TOOLS_DIR"
+echo "NUGET_URL: $NUGET_URL"
+echo "CAKE_PROPS_PATH: $CAKE_PROPS_PATH"
+echo "CAKE_ARGUMENTS: ${CAKE_ARGUMENTS[*]}"
+
+###########################################################################
+# RESTORE CAKE AND LIBS
+###########################################################################
+
 # Make sure the tools folder exist.
 if [ ! -d "$TOOLS_DIR" ]; then
   mkdir "$TOOLS_DIR"
 fi
 
-# Make sure that packages.config exist.
-if [ ! -f "$TOOLS_DIR/packages.config" ]; then
-    echo "Downloading packages.config..."
-    curl -Lsfo "$TOOLS_DIR/packages.config" https://cakebuild.net/download/bootstrapper/packages
-    if [ $? -ne 0 ]; then
-        echo "An error occurred while downloading packages.config."
-        exit 1
-    fi
+# Write cake.props to tools folder.
+if [ ! -f "$CAKE_PROPS_PATH" ]
+then
+    echo "cake.props doesnot exists"
+    cat > "$CAKE_PROPS_PATH" <<EOL
+<Project Sdk="Microsoft.NET.Sdk">
+<PropertyGroup>
+  <TargetFramework>netcoreapp3.0</TargetFramework>
+</PropertyGroup>
+<ItemGroup>
+  <PackageReference Include="MicroElements.DevOps" Version="$DEVOPS_VERSION" />
+</ItemGroup>
+</Project>
+EOL
+    echo "cake.props written to $CAKE_PROPS_PATH"
+    cat "$CAKE_PROPS_PATH"
 fi
 
-# Download NuGet if it does not exist.
-if [ ! -f "$NUGET_EXE" ]; then
-    echo "Downloading NuGet..."
-    curl -Lsfo "$NUGET_EXE" https://dist.nuget.org/win-x86-commandline/latest/nuget.exe
-    if [ $? -ne 0 ]; then
-        echo "An error occurred while downloading nuget.exe."
-        exit 1
-    fi
-fi
-
-# Restore tools from NuGet.
-pushd "$TOOLS_DIR" >/dev/null
-if [ ! -f "$PACKAGES_CONFIG_MD5" ] || [ "$( cat "$PACKAGES_CONFIG_MD5" | sed 's/\r$//' )" != "$( $MD5_EXE "$PACKAGES_CONFIG" | awk '{ print $1 }' )" ]; then
-    find . -type d ! -name . ! -name 'Cake.Bakery' | xargs rm -rf
-fi
-
-mono "$NUGET_EXE" install -ExcludeVersion
-if [ $? -ne 0 ]; then
-    echo "Could not restore NuGet tools."
-    exit 1
-fi
-
-$MD5_EXE "$PACKAGES_CONFIG" | awk '{ print $1 }' >| "$PACKAGES_CONFIG_MD5"
-
-popd >/dev/null
-
-# Restore addins from NuGet.
-if [ -f "$ADDINS_PACKAGES_CONFIG" ]; then
-    pushd "$ADDINS_DIR" >/dev/null
-
-    mono "$NUGET_EXE" install -ExcludeVersion
-    if [ $? -ne 0 ]; then
-        echo "Could not restore NuGet addins."
-        exit 1
-    fi
-
-    popd >/dev/null
-fi
-
-# Restore modules from NuGet.
-if [ -f "$MODULES_PACKAGES_CONFIG" ]; then
-    pushd "$MODULES_DIR" >/dev/null
-
-    mono "$NUGET_EXE" install -ExcludeVersion
-    if [ $? -ne 0 ]; then
-        echo "Could not restore NuGet modules."
-        exit 1
-    fi
-
-    popd >/dev/null
-fi
-
-# Make sure that Cake has been installed.
-if [ ! -f "$CAKE_EXE" ]; then
-    echo "Could not find Cake.exe at '$CAKE_EXE'."
-    exit 1
-fi
+# Restore Cake and Packages
+dotnet restore $CAKE_PROPS_PATH --packages $TOOLS_DIR --source "$NUGET_URL"
+dotnet tool restore
 
 # Start Cake
-exec mono "$CAKE_EXE" $SCRIPT "${CAKE_ARGUMENTS[@]}"
+echo "Running build script..."
+CMD="dotnet cake $SCRIPT ${CAKE_ARGUMENTS[@]}"
+echo $CMD
+exec $CMD
